0427f2eec3b1ea9185da02e25f8d962da956a6e1
Rafka Imanda Putra <rafka.0312@gmail.com>
2022-09-20 14:58:53 +0700

    add new design style

--- app/src/main/java/com/raffcodegallery/sweetly/MainActivity.kt
+++ app/src/main/java/com/raffcodegallery/sweetly/MainActivity.kt
@@ -17,10 +17,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var mood: ImageView = findViewById(R.id.moodGen)
-        var logout: TextView = findViewById(R.id.kembali)
         firebaseAuth = FirebaseAuth.getInstance()
 
+        var logout: TextView = findViewById(R.id.kembali)
         logout.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             val intent = Intent(this, SignInActivity::class.java)
@@ -28,24 +27,27 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        var mood: ImageView = findViewById(R.id.moodGen)
         mood.setOnClickListener {
             startActivity(Intent(this, MoodBoosterFormActivity::class.java))
         }
 
         var birthday: ImageView = findViewById(R.id.birthGen)
-
         birthday.setOnClickListener {
             startActivity(Intent(this, BirthdayGenActivity::class.java))
         }
 
         var mood_level: ImageView = findViewById(R.id.moodLevel)
-
         mood_level.setOnClickListener {
             startActivity(Intent(this, RecycleLayout::class.java))
         }
 
-        var about: ImageView = findViewById(R.id.about)
+        var musicZone: ImageView = findViewById(R.id.musicZone)
+        musicZone.setOnClickListener {
+            startActivity(Intent(this, MusicZone::class.java))
+        }
 
+        var about: TextView = findViewById(R.id.about)
         about.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://www.instagram.com/raff_imanda")))
         }
--- app/src/main/java/com/raffcodegallery/sweetly/MusicZone.kt
+++ app/src/main/java/com/raffcodegallery/sweetly/MusicZone.kt
@@ -1,11 +1,19 @@
 package com.raffcodegallery.sweetly
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_music_zone.*
 
 class MusicZone : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_zone)
+
+        kembali.setOnClickListener {
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+        }
     }
 }
