606ab70b8e0ab5946b469f146637b8a32126a1ad
Rafka Imanda Putra <rafka.0312@gmail.com>
2022-09-20 14:58:44 +0700

    add new design style

---
+++ app/src/main/java/com/raffcodegallery/sweetly/MusicZone.kt
@@ -0,0 +1,11 @@
+package com.raffcodegallery.sweetly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicZone : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_zone)
+    }
+}
